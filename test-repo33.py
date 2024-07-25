a33a6bb773812a3d7a33d86e6777a08948fbab49
Michael1182mitnick <56063336+Michael1182mitnick@users.noreply.github.com>
2024-07-24 22:16:15 -0800

    Create test-repo33.py

---
+++ test-repo33.py
@@ -0,0 +1,9 @@
+# Write a Python program to sort a list of numbers in ascending order.
+
+list_number = [5, 3, 6, 9, 1]
+
+# using .sort method
+list_number.sort()
+
+# Now we print the list_number
+print(list_number)
